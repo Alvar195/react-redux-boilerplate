fec69c425f2789e47a4ddec4b769619b774bc519
martimutso <marti.mutso@gmail.com>
2018-07-02 15:32:03 +0300

    initial files and folder structure with some example code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import example from './store/reducers/example';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  example: example
+});
+
+const store = createStore(rootReducer);
+
+const app = (
+  <Provider store={store}>
+    <App/>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
